1b91d9497a443895c90f4f39d7bfce83275af58b
Jani Patokallio <jani.patokallio@lonelyplanet.com>
2011-04-20 15:57:40 +1000

    More accurate way of ensuring we have Hash.to_xml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,10 @@ Spec::Runner.configure do |config|
   config.mock_with RR::Adapters::Rspec
 end
 
-require 'active_support/version'
-if ActiveSupport::VERSION::MAJOR >= 3
-  # :nodoc: Rails 3: #to_xml is defined in ActiveModel::Serializers::Xml
+# :nodoc: Rails 2.3.x: Hash#to_xml is defined in active_support
+# :nodoc: Rails 3: #to_xml is defined in ActiveModel::Serializers::Xml
+require 'active_support'
+unless Hash.method_defined? :to_xml
   require 'active_model'
   Hash.send(:include, ActiveModel::Serializers::Xml)
-else
-  # :nodoc: Rails 2.3.x: Hash#to_xml is defined in active_support
-  require 'active_support'
 end
